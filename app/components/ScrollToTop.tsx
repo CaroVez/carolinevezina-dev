ad14a4365ea44ab1c3e205b5db58be9ba4fe56e4
Caroline Vézina <caroline_vezina@hotmail.com>
2026-01-21 12:01:08 -0500

    cursor pointer on scroll to top

--- app/components/ScrollToTop.tsx
+++ app/components/ScrollToTop.tsx
@@ -23,7 +23,7 @@ export default function ScrollToTop() {
   return (
     <button
       onClick={scrollToTop}
-      className={`fixed bottom-24 right-6 z-50 p-3 rounded-full shadow-lg transition-all duration-500 border text-[#479796] border-[#479796] bg-[#f2f2f2]/60 backdrop-blur-md hover:border-[#ba7954] hover:bg-[#ba7954] hover:text-white hover:border-[#ba7954] group
+      className={`fixed bottom-24 right-6 z-50 p-3 rounded-full shadow-lg transition-all duration-500 border text-[#479796] border-[#479796] bg-[#f2f2f2]/60 backdrop-blur-md hover:border-[#ba7954] hover:bg-[#ba7954] hover:text-white hover:border-[#ba7954] cursor-pointer group
       ${isVisible ? "opacity-100 " : "opacity-0  pointer-events-none"}`}
       aria-label="Retour en haut"
     >
