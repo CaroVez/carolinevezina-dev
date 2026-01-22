bf31d4b2ba66a48065e0c4315efabbd5b6a351b1
Caroline Vézina <caroline_vezina@hotmail.com>
2026-01-22 11:43:08 -0500

    favicon

--- app/root.tsx
+++ app/root.tsx
@@ -26,6 +26,21 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
   { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png",
+  },
+  { rel: "manifest", href: "/site.webmanifest" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -68,6 +83,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="msapplication-TileColor" content="#479796" />
+        <meta name="theme-color" content="#479796" />
         <Meta />
         <Links />
       </head>
